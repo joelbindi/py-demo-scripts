f08cc8d6dcc797d663a101d2fb6a30ea659e6f40
Joel Bindi <joel@hitch.mobi>
2016-09-11 11:38:00 +0800

    working socket from this directory

--- search.py
+++ search.py
@@ -1,16 +1,24 @@
 import re 
 
 #Old way
-# fhand = open('mbox-short.txt')
-
-# for line in fhand: 
-# 	if line.find('From: ') >= 0 :
-# 		print line
-
-#using Regex
 fhand = open('mbox-short.txt')
 
 for line in fhand: 
-	line = line.strip()
-	if re.search('From: ', line) >= 0:
+	if line.find('From: ') >= 0 :
 		print line
+
+#using Regex
+# fhand = open('mbox-short.txt')
+
+for line in fhand: 
+	line = line.strip()
+	if re.search('^X-', line) >= 0:
+		print line
+
+x = 'my fave numbres are 4,5 and 8'
+y = re.findall('[0-9]+',x)
+print y
+
+x = 'From: Using the : character'
+y = re.findall('\$', x)
+print y
